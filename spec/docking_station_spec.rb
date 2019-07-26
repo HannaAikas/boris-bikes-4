77ab2532562ddc562e73adfd91cdad9e5f786f07
Guillaume <liard.guillaume@gmail.com>
2019-07-26 16:21:09 +0100

    added test: docking station should not accept bikes for docking when full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,16 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
     it 'responds to release_bike' do
         expect(subject).to respond_to :release_bike
     end
     # THE BELOW TEST HAS BEEN COMMENTED OUT BECAUSE IT WORKED IN STEP 10 (WHEN release_bike CREATED A NEW INSTANCE OF BIKE) BUT NO LONGER WORKS IN STEP 12
-    # it 'releases working bikes' do
-    #     bike = subject.release_bike
-    #     expect(bike).to be_working
-    # end
+    it 'releases working bikes' do
+        subject.dock(Bike.new)
+        bike = subject.release_bike
+        expect(bike).to be_working
+    end
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it { is_expected.to respond_to(:bike) }
@@ -37,4 +39,11 @@ describe DockingStation do
 
     end
 
+    describe '#dock' do
+        it "does not allow to dock if station is full" do
+            subject.dock(Bike.new)
+            expect { subject.dock(Bike.new) }.to raise_error "Station full, cannot dock bike."
+        end
+    end
+        
 end
